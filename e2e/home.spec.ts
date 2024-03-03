695e21ddeb5c792e295dcbe86a7b4524e78aadb4
Anayat Khan <anayat0khan@gmail.com>
2024-03-03 20:16:40 +0530

    next js template

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
-test.describe("landing Page", () => {
+test.describe("landing", () => {
     test("landing page", async ({ page }) => {
         await page.goto("/");
     });
